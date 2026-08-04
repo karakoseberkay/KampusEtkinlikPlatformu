825015b8d7026602a202158902d89047a44332c2
Berkay Karaköse <berkaykarakose00@gmail.com>
2026-08-04 17:17:55 +0300

    chore: initialize Angular and ASP.NET Core projects

---
+++ backend/KampusEtkinlik.Api/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint(
+            "/openapi/v1.json",
+            "Kampüs Etkinlik API v1"
+        );
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
